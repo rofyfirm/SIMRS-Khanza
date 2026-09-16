ef9098d9f148dd882f05f2ecd9510f0843c588f0
rofyfirm <slope4741@gmail.com>
2026-09-16 21:10:09 +0700

    change version
    change version

--- src/simrskhanza/frmUtama.java
+++ src/simrskhanza/frmUtama.java
@@ -7482,7 +7482,7 @@ public class frmUtama extends javax.swing.JFrame {
         lblUser1.setFont(new java.awt.Font("Tahoma", 0, 11)); // NOI18N
         lblUser1.setForeground(new java.awt.Color(50, 50, 50));
         lblUser1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lblUser1.setText("826.v3.1");
+        lblUser1.setText("826.v4");
         lblUser1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         lblUser1.setName("lblUser1"); // NOI18N
         lblUser1.setPreferredSize(new java.awt.Dimension(170, 23));
